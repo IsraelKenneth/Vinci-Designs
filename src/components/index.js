14f00fcef0602e7ed061c1282f9c8b9b4e1a7b75
Israel Kenneth <42800016+IsraelKenneth@users.noreply.github.com>
2023-03-20 01:52:25 +0100

    src

---
+++ src/components/index.js
@@ -0,0 +1,12 @@
+import About from "./About"
+import Experience from "./Experience"
+import Feedbacks from "./Feedbacks"
+import Hero from "./Hero"
+import Loader from "./Loader"
+import Navbar from "./Navbar"
+import Tech from "./Tech"
+import Works from "./Works"
+import Contact from "./Contact"
+
+
+export { About, Experience, Feedbacks, Hero, Loader, Navbar, Tech, Works, Contact }
